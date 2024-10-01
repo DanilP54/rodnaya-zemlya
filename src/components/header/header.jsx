5d7215b1f558351b55c8a8b46f5efd68e81c44a5
DanilP54 <putrodan@gmail.com>
2024-10-01 22:14:31 +0500

    +

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,6 @@ import LogoImage from '../../../public/logo.png';
 import { Link } from 'react-router-dom';
 import { Tooltip } from '@mantine/core';
 import { Spotlight, spotlight } from '@mantine/spotlight';
-import { Kbd } from '@mantine/core';
 
 function IconSearch() {
     return <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
@@ -111,23 +110,7 @@ export function Header() {
                             <span style={{
                                 fontSize: '15px',
                             }}>Search</span>
-                            <div style={{
-                                marginLeft: 'auto'
-                            }}>
-                                <Kbd style={{
-                                    padding: '3px',
-                                    backgroundColor: 'transparent',
-                                    color: 'rgb(33, 32, 32)',
-                                    fontSize: '10px',
-                                    fontWeight: 'bold',
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    justifyContent: 'center'
-                                    // height: '90%',
-                                }} size='xs'>Ctrl + K</Kbd>
-                            </div>
                         </button>
-                        {/* <Input fluid onClick={spotlight.open} icon='search' placeholder='Search for artist, album or track' /> */}
                         <Spotlight
                             actions={actions}
                             nothingFound="Nothing found..."
