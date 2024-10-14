6a554875e6ef6483888bac1f5a81e70486b089bd
DanilP54 <putrodan@gmail.com>
2024-10-14 20:38:00 +0500

    +

--- src/components/pages/news/tabs/Tabs.jsx
+++ src/components/pages/news/tabs/Tabs.jsx
@@ -1,41 +1,22 @@
 import { Tabs, Text } from '@mantine/core';
+import styles from './tabs.module.css'; // Подключаем CSS модуль
 
 import Post from '../post/Post';
 import AuthButtons from '../../../header/AuthButtons';
 export default function TabsComponents({ setTabs }) {
 
-
     return (
         <Tabs h={'100%'} defaultValue="our news">
-            <Tabs.List h={'60px'} style={{
-                backgroundColor: 'transparent'
-            }}>
-                <Tabs.Tab value="our news" style={{
-                    fontSize: '17px',
-                    fontWeight: '500',
-                    color: 'white',
-                    backgroundColor: 'transparent'
-                }} >
+            <Tabs.List h={'60px'} className={styles.tabsList}> {/* Применяем класс из CSS модуля */}
+                <Tabs.Tab value="our news" className={styles.tab}>
                     Наша лента
                 </Tabs.Tab>
-                <Tabs.Tab value="your news" style={{
-                    fontSize: '17px',
-                    fontWeight: '500',
-                    color: 'white',
-                    backgroundColor: 'transparent'
-                }}  >
+                <Tabs.Tab value="your news" className={styles.tab}>
                     Ваша лента
                 </Tabs.Tab>
             </Tabs.List>
 
-            <Tabs.Panel style={{
-                height: 'calc(100vh - 60px)',
-                overflow: 'auto',
-                padding: '10px',
-                display: 'flex',
-                flexDirection: 'column',
-                gap: '30px'
-            }} value="our news">
+            <Tabs.Panel className={styles.panel} value="our news"> {/* Применяем класс из CSS модуля */}
                 <Post setTabs={setTabs} src={'https://api.dicebear.com/9.x/dylan/svg?seed=Eliza'} />
                 <Post setTabs={setTabs} src={'https://api.dicebear.com/9.x/dylan/svg?seed=Christian'} />
                 <Post setTabs={setTabs} src={'https://api.dicebear.com/9.x/dylan/svg?seed=Destiny'} />
@@ -43,26 +24,11 @@ export default function TabsComponents({ setTabs }) {
                 <Post setTabs={setTabs} src={'https://api.dicebear.com/9.x/miniavs/svg?seed=Christian'} />
             </Tabs.Panel>
 
-            <Tabs.Panel style={{
-                height: 'calc(100vh - 60px)',
-                padding: '10px',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center'
-            }} value="your news">
-                <div style={{
-                    color: 'white',
-                    boxShadow: '0 0 3px 5px rgba(0,0,0, .2)',
-                    backgroundColor: 'rgba(0,0,0, .2)',
-                    padding: '20px',
-                    borderRadius: '5px'
-                }}>
-                    <Text fw={500} size='lg' c={'cyan'}  >Хочешь мутить тут делишки свои?</Text>
-                    <Text fw={500} size='md' c={'lime'}  >Тогда тебе надо зарегаться, братик, такие условия.</Text>
-                    <div style={{
-                        marginTop: '10px',
-                    }}>
-
+            <Tabs.Panel className={styles.panel_two} value="your news"> {/* Применяем класс из CSS модуля */}
+                <div className={styles.authContainer}>
+                    <Text fw={500} size='lg' c={'cyan'}>Хочешь мутить тут делишки свои?</Text>
+                    <Text fw={500} size='md' c={'lime'}>Тогда тебе надо зарегаться, братик, такие условия.</Text>
+                    <div style={{ marginTop: '10px' }}>
                         <AuthButtons />
                     </div>
                 </div>
